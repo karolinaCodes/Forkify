a211bcb1f2b63c98804e487ef864f4f1e72e9b8d
Karswis <karolinaswislocki@hotmail.com>
2021-04-21 14:45:34 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// using all caps for config variable name is a convention
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4e3b9fa0-2533-4648-9ab6-e2d41697861b';
+export const MODAL_CLOSE_SEC = 2.5;
